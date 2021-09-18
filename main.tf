463895fdd7821b52e27532b2668835fb5d59cbf1
Rahul Tandon <rahul.tandon1000@gmail.com>
2021-09-19 00:24:29 +0530

    Initital Checkin

---
+++ main.tf
@@ -0,0 +1,41 @@
+provider "azurerm"{
+    features {
+      
+    }
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf_rg_blobstore"
+    storage_account_name = "tfstoreaccountrayimatics"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
+resource "azurerm_resource_group" "tf_test"{
+    name     = "tfmainrg"
+    location = "Central US"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+    name                = "weatherapi"
+    location            = azurerm_resource_group.tf_test.location
+    resource_group_name = azurerm_resource_group.tf_test.name
+    ip_address_type     = "public"
+    dns_name_label      = "rayimaticswa"
+    os_type             = "Linux"
+
+    container{
+        name            = "weatherapi"
+        image           = "rayimatics/weatherapi"
+        cpu             = "1"
+        memory          = "1"
+
+        ports{
+            port       = 80
+            protocol    = "TCP"            
+        }
+    }
+}
