80a39e8fa1703b960c14cbf3fe1c3e7a3f9d4040
Rahul Tandon <rahul.tandon1000@gmail.com>
2021-09-19 01:45:08 +0530

    add version tag

--- main.tf
+++ main.tf
@@ -19,6 +19,12 @@ resource "azurerm_resource_group" "tf_test"{
     location = "Central US"
 }
 
+variable "imagebuild" {
+  type        = string
+  description = "Latest Image Build "
+}
+
+
 resource "azurerm_container_group" "tfcg_test" {
     name                = "weatherapi"
     location            = azurerm_resource_group.tf_test.location
@@ -29,7 +35,7 @@ resource "azurerm_container_group" "tfcg_test" {
 
     container{
         name            = "weatherapi"
-        image           = "rayimatics/weatherapi"
+        image           = "rayimatics/weatherapi:{var.imagebuild}"
         cpu             = "1"
         memory          = "1"
 
