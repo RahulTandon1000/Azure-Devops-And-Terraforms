3a3bf439bf309a6952dd9ddfd755bf40cbaad377
Rahul Tandon <rahul.tandon1000@gmail.com>
2021-09-19 23:49:03 +0530

    added $  to main.tf

--- main.tf
+++ main.tf
@@ -14,15 +14,15 @@ terraform {
   }
 }
 
+variable "imagebuild" {
+  type        = string
+  description = "Latest Image Build "
+}
+
 
 resource "azurerm_resource_group" "tf_test"{
     name     = "tfmainrg"
     location = "Central US"
-}
-
-variable "imagebuild" {
-  type        = string
-  description = "Latest Image Build "
 }
 
 
@@ -30,13 +30,14 @@ resource "azurerm_container_group" "tfcg_test" {
     name                = "weatherapi"
     location            = azurerm_resource_group.tf_test.location
     resource_group_name = azurerm_resource_group.tf_test.name
+
     ip_address_type     = "public"
     dns_name_label      = "rayimaticswa"
     os_type             = "Linux"
 
     container{
         name            = "weatherapi"
-        image           = "rayimatics/weatherapi:{var.imagebuild}"
+        image           = "rayimatics/weatherapi:${var.imagebuild}"
         cpu             = "1"
         memory          = "1"
 
